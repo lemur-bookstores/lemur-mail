a28417176623917369893712e9764c4ed0578de3
Elkin Portocarrero <elkincp5@gmail.com>
2025-05-21 00:33:02 -0500

    exportacion de toda la libreria

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { TemplateConfigManager } from "./configs/templateConfigManager ";
+export { Smtp, Gmail, MailGun, Providers } from "./providers";
+export { SendTrigger } from "./sendtrigger";
+export { MailBuilder } from "./build";
